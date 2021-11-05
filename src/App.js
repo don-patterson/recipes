f9c491abcd6e92b94119c55d446740d8dddc9bf2
Donald Patterson <donald.patterson@gmail.com>
2021-11-05 16:25:23 -0300

    using marked+github API

--- src/App.js
+++ src/App.js
@@ -1,37 +1,20 @@
 import React, {useState} from "react";
 import {Button} from "@material-ui/core";
 import marked from "marked";
+import {getContents} from "@deek80/gh-reader";
 
 export default () => {
-  const [items, setItems] = useState([]);
-  const [markdown, setMarkdown] = useState("");
+  const [markdown, setMarkdown] = useState("nothing loaded yet");
 
-  const doFetch = async () => {
-    setItems(await ls("deek80", "recipes", "/public/list"));
-  };
-
-  const dumpContent = async url => {
-    const response = await fetch(url);
-    if (!response.ok) {
-      console.error("Failed to fetch url:", url);
-      setMarkdown("Failed to fetch url");
-    }
-    setMarkdown(await response.text());
+  const loadRecipePoc = async () => {
+    const contents = await getContents("deek80", "recipes", "public/list");
+    const file_response = await fetch(contents.response[0].download_url);
+    setMarkdown(await file_response.text());
   };
 
   return (
     <>
-      <div>
-        Api limits: remaining:{remaining} minutesUntilReset:{minutesUntilReset}
-      </div>
-      <Button onClick={doFetch} variant="contained">
-        Do Fetch
-      </Button>
-      {items.map(item => (
-        <Button key={item.path} onClick={() => dumpContent(item.download_url)}>
-          {item.path}
-        </Button>
-      ))}
+      <Button onClick={loadRecipePoc}>Load recipe</Button>
       <div dangerouslySetInnerHTML={{__html: marked(markdown)}}></div>
     </>
   );
