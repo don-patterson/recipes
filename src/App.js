fb42ab4e6271d5f2442b4f1084b37e259c23a9c5
Donald Patterson <donald.patterson@gmail.com>
2021-11-11 19:52:17 -0400

    update POC to use new Repo class

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React, {useState} from "react";
 import {Button} from "@material-ui/core";
 import marked from "marked";
-import {getContents} from "@deek80/gh-reader";
+import {Repo} from "@deek80/gh-reader";
+
+const repo = new Repo("deek80/recipes");
 
 export default () => {
   const [markdown, setMarkdown] = useState("nothing loaded yet");
 
+  const download = async file => {
+    const httpResponse = await fetch(file.download_url);
+    if (!httpResponse.ok) {
+      console.error("Failed to fetch:", file.download_url);
+      return null;
+    }
+    return httpResponse.text();
+  };
+
   const loadRecipePoc = async () => {
-    const contents = await getContents("deek80", "recipes", "public/list");
-    const file_response = await fetch(contents.response[0].download_url);
-    setMarkdown(await file_response.text());
+    const files = await repo.ls("public/list");
+    console.log("got files:", files);
+    const firstRecipe = await download(files[0]);
+    setMarkdown(firstRecipe);
   };
 
   return (
